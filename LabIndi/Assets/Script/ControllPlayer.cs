fd97232e279f20a5b357fb33b8bef7fa0265d777
NerdyCep <cfyz.2000.cfyz.2000@yandex.ru>
2023-08-04 17:48:34 +0300

    comitMac
    добавил управление
    вид сверху
    без привязки камеры

--- LabIndi/Assets/Script/ControllPlayer.cs
+++ LabIndi/Assets/Script/ControllPlayer.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class ControllPlayer : MonoBehaviour
 {
+    private CharacterController characterController;
+    [SerializeField] private float _speed = 5f;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
     private void Update()
     {
-  
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
+        characterController.Move(move * Time.deltaTime * _speed);
     }
 
 
